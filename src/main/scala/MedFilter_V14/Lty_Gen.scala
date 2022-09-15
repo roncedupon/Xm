1667b3bc2dbac9e3998777af4df39bc66e910dba
roucedupon <2507317801@qq.com>
2022-09-15 23:35:38 +0800

    处理完了毛刺，也基本解决了数据输出问题，现在还有一个关于数据输出的问题：开头多一个和结尾少一个的问题，mvalid不匹配的问题

--- src/main/scala/MedFilter_V14/Lty_Gen.scala
+++ src/main/scala/MedFilter_V14/Lty_Gen.scala
@@ -28,7 +28,7 @@ class Lty_Bram extends BlackBox{//黑盒，入32bit，出16 bit
     mapCurrentClockDomain(io.clkb)
 }
 object LTY_ENUM extends SpinalEnum(defaultEncoding = binaryOneHot) {
-  val IDLE, INIT, LOAD_2_ROWS,EXTRACT_LTY,JUDGE_LAST_ROW,WAIT_NEXT_READY = newElement
+  val IDLE, INIT, LOAD_2_ROWS,EXTRACT_LTY,JUDGE_LAST_ROW = newElement
   //WAIT_NEXT_READY：等待下一个模块ready（可以认为LTY参数累加模块ready了）
   //Judge Last Row:每一行结束了都需要启动最后一行的判断
 }
@@ -44,7 +44,7 @@ case class LTY_FSM(start: Bool) extends Area {
   val Row_2_End=Bool()//前两行加载完
   val Col_End=Bool()//输出完一行,这里要注意，这是输出完一行的意思
   val Load_2_Row_End=Bool()//加载完两行
-  val Next_Ready=Bool()//累加模块准备好接受数据，ready了
+
   val Judge2Row_Col_End=Bool()//判断加载前两行的一行是否结束
   switch(currentState){
     is(LTY_ENUM.IDLE){
@@ -72,22 +72,23 @@ case class LTY_FSM(start: Bool) extends Area {
         when(last_Row){
             nextState:=LTY_ENUM.IDLE//输出完最后一行
         }elsewhen(Load_2_Row_End){
-            nextState:=LTY_ENUM.WAIT_NEXT_READY//等待下面的计算模块准备好接受计算数据
+            nextState:=LTY_ENUM.EXTRACT_LTY//等待下面的计算模块准备好接受计算数据
         }otherwise{
             nextState:=LTY_ENUM.LOAD_2_ROWS//前两行没加载完继续加载前两行
         }
     }
-    is(LTY_ENUM.WAIT_NEXT_READY){
-        when(Next_Ready){
-            nextState:=LTY_ENUM.EXTRACT_LTY
-        }otherwise{
-            nextState:=LTY_ENUM.WAIT_NEXT_READY//下一个模块没准备好，就一直等着
-        }
-    }
+    // is(LTY_ENUM.WAIT_NEXT_READY){
+    //     when(Next_Ready){
+    //         nextState:=LTY_ENUM.EXTRACT_LTY
+    //     }otherwise{
+    //         nextState:=LTY_ENUM.WAIT_NEXT_READY//下一个模块没准备好，就一直等着
+    //     }
+    // }
+    //由于进数据和出数据是独立的，无需考虑数据的反压，所以无需这个等下一层好的状态
+    //只要在Extract Lty状态，那么数据就能valid，只要下一层ready，那么数据就能流下去
     is(LTY_ENUM.EXTRACT_LTY){
-        when(!Next_Ready){
-            nextState:=LTY_ENUM.WAIT_NEXT_READY
-        }elsewhen(Col_End){
+        when(Col_End){//只要进入这个状态，我们就可以保证数据要处理的那两行数据已经缓存完了，
+            //接下来要做的就是正确地将数据输出去更新连通域标记矩阵
             nextState:=LTY_ENUM.JUDGE_LAST_ROW
         }otherwise{
             nextState:=LTY_ENUM.EXTRACT_LTY
@@ -112,10 +113,10 @@ class Lty_Mark_Gen extends Component{//连通域标记
     noIoPrefix()
     val Fsm=LTY_FSM(io.start)
 //状态机相关====================================================================
-    val Data_Out_Flag=Fsm.currentState===LTY_ENUM.EXTRACT_LTY||Fsm.currentState===LTY_ENUM.WAIT_NEXT_READY
+    //val Data_Out_Flag=Fsm.currentState===LTY_ENUM.EXTRACT_LTY||Fsm.currentState===LTY_ENUM.WAIT_NEXT_READY
     //不用这个↑作为mValid的判断是因为mValid标识出来的输出数据开头第一个会重复一下，结尾会少一个
     val Col_Cnt=WaCounter(io.sData.valid&&io.sData.ready, log2Up(Config.LTY_DATA_BRAM_A_DEPTH), Config.LTY_DATA_BRAM_A_DEPTH-1)//创建输入数据的列计数器
-    val Bram_Out_Cnt=WaCounter(io.mData2.fire, log2Up(Config.LTY_DATA_BRAM_B_DEPTH), Config.LTY_DATA_BRAM_B_DEPTH-1)//创建输出数据的列计数器
+    val Bram_Out_Cnt=WaCounter(io.mData2.ready&&Fsm.currentState===LTY_ENUM.EXTRACT_LTY, log2Up(Config.LTY_DATA_BRAM_B_DEPTH), Config.LTY_DATA_BRAM_B_DEPTH-1)//创建输出数据的列计数器
     val Bram_Out_Row_Cnt=WaCounter(Bram_Out_Cnt.valid, log2Up(Config.LTY_ROW_NUM/2),Config.LTY_ROW_NUM/2-1)//创建输出行数计数器,记得除2，因为并行度是2
     val INIT_CNT=WaCounter(Fsm.currentState === LTY_ENUM.INIT, 3, 5)//初始化计数器,数五拍
     val Row_Cnt_All=WaCounter(Col_Cnt.valid,log2Up(Config.LTY_ROW_NUM),Config.LTY_ROW_NUM-1)//行计数器
@@ -128,19 +129,20 @@ class Lty_Mark_Gen extends Component{//连通域标记
         //于是在状态机里又加了一些其他的判断
     Fsm.Judge2Row_Col_End:=Col_Cnt.valid//缓存完开头两行中的一行
     Fsm.Load_2_Row_End:=Row_Cnt_All.count>1//开头两行被缓存完了，行计数器为2的时候那么就加载完两行了,不能大于等于一，因为加载完第0行后，Row_Cnt就已经是1了
-    Fsm.Next_Ready:=io.mData2.ready   
+ 
     Fsm.Col_End:=Bram_Out_Cnt.valid//发完了一行数据，也就是发送数据计数器数完了2040，表面上发送完一行，实际上处理完了两行
 //一个Bram写选择器，一个Bram读选择器
-    val Bram_Read_Chose=Reg(UInt(1 bits))init(0)//要准确控制才行，是读0，1还是2，3
+    val Bram_Read_Chose=Reg(Bool())init(False)//要准确控制才行，是读0，1还是2，3
     //4个Bram的作用是在计算前两行的时候加载后两行数据
     when(Bram_Out_Cnt.valid){
-        Bram_Read_Chose:=Bram_Read_Chose+1
+        Bram_Read_Chose:=(!Bram_Read_Chose)
     }
    //违背逻辑的原因是避免在导入前两行的时候输出
     //也就是说，在加载第3，4行的时候，输出1，2行，加载1，2行时同理
-//创建标记矩阵---并行度为2，所以也得要两个Ram作为上标记矩阵=====================================
+    //创建标记矩阵---并行度为2，所以也得要两个Ram作为上标记矩阵=====================================
     val Up_Mark_Mem1=Mem(UInt(Config.LTY_MARK_BRAM_WIDTH bits),wordCount=Config.LTY_MARK_BRAM_DEPTH)//标记矩阵
     val Up_Mark_Mem2=Mem(UInt(Config.LTY_MARK_BRAM_WIDTH bits),wordCount=Config.LTY_MARK_BRAM_DEPTH)//标记矩阵
+
 //建立四个数据缓存Bram=======================================================================
     val Wr_En=Vec(Bool(),4)//循环写
     for(i<-0 to 3){
@@ -164,14 +166,9 @@ class Lty_Mark_Gen extends Component{//连通域标记
         FeatureMem(i).io.addrb:=Bram_Out_Cnt.count//读地址待处理
         FeatureMem(i).io.enb:=True//以后有需要在处理
     }
-    io.mData1.payload:=Bram_Read_Chose.mux(//输出数据选择器
-        0->FeatureMem(0).io.doutb,
-        1->FeatureMem(2).io.doutb
-    )
-    io.mData2.payload:=Bram_Read_Chose.mux(//输出数据选择器
-        0->FeatureMem(1).io.doutb,
-        1->FeatureMem(3).io.doutb
-    )//mux必须是圆括号
+    io.mData1.payload:=Bram_Read_Chose?FeatureMem(2).io.doutb|FeatureMem(0).io.doutb//输出数据选择器
+    io.mData2.payload:=Bram_Read_Chose?FeatureMem(3).io.doutb|FeatureMem(1).io.doutb//输出数据选择器
+//mux必须是圆括号
 //控制数据输入输出
 
 
@@ -180,7 +177,7 @@ class Lty_Mark_Gen extends Component{//连通域标记
     when(Fsm.currentState===LTY_ENUM.JUDGE_LAST_ROW&&(!io.sData.ready)){//为毛线要取个反？我敲，我忘记为啥了
         Row_Cnt_2.clear//每缓存完一行数据都要进行是否是最后一行判断
     }
-    when(Fsm.currentState===LTY_ENUM.EXTRACT_LTY||Fsm.currentState===LTY_ENUM.WAIT_NEXT_READY){//这里加RegNext的原因：对波形时开头多了一个点结尾少了一个点，待分析
+    when(Fsm.currentState===LTY_ENUM.EXTRACT_LTY){//这里加RegNext的原因：对波形时开头多了一个点结尾少了一个点，待分析
         io.mData1.valid:=True//添加后面的Fsm.currentState===LTY_ENUM.WAIT_NEXT_READY条件是因为没必要让mValid在连通域提取状态拉高拉低
         io.mData2.valid:=True
     }otherwise{
@@ -192,6 +189,7 @@ class Lty_Mark_Gen extends Component{//连通域标记
 
 }
 class Lty_Mark_Sub_Module extends Component{//标记子模块
+    val Config=MemConfig()
     val io=new Bundle{
         val Up_mark=in UInt(16 bits)//上标记
 
@@ -217,7 +215,6 @@ class Lty_Mark_Sub_Module extends Component{//标记子模块
 class Lty_Para_Accu extends Component{
     //连通域参数累加
 }
-
 
 object LtyGen extends App { 
     val verilog_path="./testcode_gen/Lty_Gen" 
